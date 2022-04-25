40bffe68b7f8d8166575329cfd21adb45be504e0
sofiapasquini <65981514+sofiapasquini@users.noreply.github.com>
2022-04-25 11:21:56 -0400

    added spectrum construction to main script

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-#import some relevant packages
-from sklearn.cluster import AgglomerativeClustering, KMeans
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_samples, silhouette_score #(?)
-import matplotlib.pyplot as plt
-from astropy.io import fits
+'''
+This is the main script in which all pre-processing, processing, 
+ML algorithms, and analysis occurs.
 
-#this will be the main script in which all pre-processing, processing, ML algorithms, and analysis will occur.
+This script is originally coded for the test Spitzer data, but will
+eventually be edited to accomodate the JWST data (perhaps a separate script?).
+
+    In the case of Spitzer data here (NGC2023- from Boersma et al 2016, Zhang et al. 2019),
+    each map has both a North and South version, for now I will just code in 
+    the script for the North maps, can revisit this concept once code is working.
+
+'''
+#import relevant packages
+from spec_build import *
+
+#load in and reshape all wavelength, extinction, spectral maps
+spectra, wave= load_spec_wavelength("NGC2023_SPECTRAL_MAP_NORTH.fits")
+
+#load in and reshape continuum and corresponding wavelentgh arrays
+cont, wave_cont=load_continuum("NGC2023_CONTINUUM_MAP_NORTH.fits")
+
+#load in and reshape the extinction maps and corresponding wavelength array
+ext, wave_ext=load_extinction("NGC2023_EXTINCTION_MAPS_NORTH.fits")
+
+#extinction correct the spectra
+ext_corr_spec=extinction_correct(ext, spectra)
--- pre-processing.py
+++
@@ -1,2 +0,0 @@
-import matplotlib.pyplot as plt
-from astropy.io import fits
