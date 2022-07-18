added pixel masking component to each algorithm, fixed some plotting errors

algo_1.py
@@ -37,7 +37,10 @@ cont, wave_cont=load_continuum("NGC2023_CONTINUUM_MAP_SOUTH.fits")
 #load in and reshape the extinction maps and corresponding wavelength array
 ext, wave_ext=load_extinction("NGC2023_EXTINCTION_MAPS_SOUTH.fits")
 
-#extinction correct the spectra
+#subtract continuum and extinction correct the spectra
+
+
+
 ##SOFIA-comment this line out when performing clustering on spectra that have
 #NOT been extinction corrected
 # ext_corr_spec=extinction_correct(ext, spectra)
@@ -46,12 +49,26 @@ ext, wave_ext=load_extinction("NGC2023_EXTINCTION_MAPS_SOUTH.fits")
 
 #transform the map(s) from 3 to 2-dimensional array(s and consolidate)
 # df=df_create(ext_corr_spec)
+
 df=df_create(spectra) #the analysis will be done (for now) on spectra NOT ext-corrected
 
-#apply normalization to the spectra
+#apply normalization to the spectra (unit norm per spectra, not to 7.7 line as previously thought)
 df=normalize(df)
 #uncomment the line below if you want to normalize wrt the 7.7 micro meter flux:
 # df=normalize_77(df)
+
+#load in and adjust the pixel mask:
+map_file= "NGC2023_ZONES_MAP_SOUTH.fits"
+hdulist = fits.open(folder+map_file)
+map=hdulist[0].data
+
+#reshape the axes to (x,y) coordinates and to 1D in order to mask out appropriate rows
+map=np.swapaxes(map, 0,1)
+map_1d=np.reshape(map, (map.shape[0]*map.shape[1],), order='c')
+to_drop=np.where(map_1d==0)[0]
+
+#remove the masked spectra from the data:
+df=mask_clean(df, map_1d)
 
 #processing- the algorithm itself
 
@@ -101,16 +118,19 @@ plt.xlabel("Number of points in node (or index of point if no parenthesis).")
 plt.show()
 
 #analyze key features of spectra groups/clusters here
-
+print(cluster_labels.shape)
 #reshape the label matrix from 1-D back to 2-D to match the spectrum matrix
-label_matrix=label_reshape(cluster_labels, spectra)
+# label_matrix=label_reshape(cluster_labels, spectra)
 spec_list=[] # an empty list to hold all of the averaged spectra
 for i in range(optimal_n_clusters):
     # spec_list.append(avg_label(i,spectra, label_matrix))
-    avg_spec=avg_label(i, spectra, label_matrix)
-    plt.plot(wave, avg_spec, label=str(i))
+    # avg_spec=avg_label(i, spectra, label_matrix)
+    avg_spec=avg_label(i,df, cluster_labels)
+    #normalize the spectra to the peak wavelength
+    norm_spec=normalize_peak(avg_spec)
+    plt.plot(wave, norm_spec, label=str(i))
 #now plot the averaged spectra together
 plt.xlabel("Wavelength [$\mu$m]")
-plt.ylabel("Flux [MJy/sr]")
+plt.ylabel("Normalized Flux")
 plt.legend()
 plt.show()

algo_2.py
@@ -14,12 +14,13 @@ eventually be edited to accomodate the JWST data (perhaps a separate script?).
 
 '''
 #import relevant packages
+from turtle import shape
 from sklearn.cluster import AgglomerativeClustering, KMeans
 from sklearn.neighbors import NearestCentroid
 from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
-from processing import optimal_clusters_inspect, pca_visual, elbow_plot, avg_label, label_reshape
+from processing import optimal_clusters_inspect, pca_visual, elbow_plot, avg_label, normalize_peak
 from spec_build import *
 from pre_processing import *
 import matplotlib.pyplot as plt
@@ -48,10 +49,24 @@ ext, wave_ext=load_extinction("NGC2023_EXTINCTION_MAPS_SOUTH.fits")
 # df=df_create(ext_corr_spec)
 df=df_create(spectra) #the analysis will be done (for now) on spectra NOT ext-corrected
 
-#apply normalization to the spectra
+#apply normalization to the spectra (unit norm per spectra, not to 7.7 line as previously thought)
 df=normalize(df)
 #uncomment the line below if you want to normalize wrt the 7.7 micro meter flux:
 # df=normalize_77(df)
+
+#load in and adjust the pixel mask:
+map_file= "NGC2023_ZONES_MAP_SOUTH.fits"
+hdulist = fits.open(folder+map_file)
+map=hdulist[0].data
+
+#reshape the axes to (x,y) coordinates and to 1D in order to mask out appropriate rows
+map=np.swapaxes(map, 0,1)
+map_1d=np.reshape(map, (map.shape[0]*map.shape[1],), order='c')
+to_drop=np.where(map_1d==0)[0]
+
+#remove the masked spectra from the data:
+df=mask_clean(df, map_1d)
+
 
 #processing- the algorithm itself
 
@@ -110,12 +125,15 @@ plt.show()
 #analyze key features of spectra groups/clusters here
 
 #reshape the label matrix from 1-D back to 2-D to match the spectrum matrix
-label_matrix=label_reshape(kmeans_cluster_labels, spectra)
+print(kmeans_cluster_labels.shape)
+# label_matrix=label_reshape(kmeans_cluster_labels, spectra)
 spec_list=[] # an empty list to hold all of the averaged spectra
 for i in range(optimal_n_clusters):
     # spec_list.append(avg_label(i,spectra, label_matrix))
-    avg_spec=avg_label(i, spectra, label_matrix)
-    plt.plot(wave, avg_spec, label=str(i))
+    avg_spec=avg_label(i, df, kmeans_cluster_labels)
+    #normalize the spectra to the peak wavelength
+    norm_spec=normalize_peak(avg_spec)
+    plt.plot(wave, norm_spec, label=str(i))
 #now plot the averaged spectra together
 plt.xlabel("Wavelength [$\mu$m]")
 plt.ylabel("Flux [MJy/sr]")

algo_3.py
@@ -54,10 +54,24 @@ ext, wave_ext=load_extinction("NGC2023_EXTINCTION_MAPS_SOUTH.fits")
 # df=df_create(ext_corr_spec)
 df=df_create(spectra) #the analysis will be done (for now) on spectra NOT ext-corrected
 
-#apply normalization to the spectra
+#apply normalization to the spectra (unit norm per spectra, not to 7.7 line as previously thought)
 df=normalize(df)
 #uncomment the line below if you want to normalize wrt the 7.7 micro meter flux:
 # df=normalize_77(df)
+
+#load in and adjust the pixel mask:
+map_file= "NGC2023_ZONES_MAP_SOUTH.fits"
+hdulist = fits.open(folder+map_file)
+map=hdulist[0].data
+
+#reshape the axes to (x,y) coordinates and to 1D in order to mask out appropriate rows
+map=np.swapaxes(map, 0,1)
+map_1d=np.reshape(map, (map.shape[0]*map.shape[1],), order='c')
+to_drop=np.where(map_1d==0)[0]
+
+#remove the masked spectra from the data:
+df=mask_clean(df, map_1d)
+
 
 #processing- the algorithm itself
 

processing.py
@@ -22,7 +22,7 @@ from scipy.cluster.hierarchy import dendrogram
 
 #SOFIA- commenting out the following import just for testing purposes! For some reason jupyter
 #does not like yellowbrick
-# from yellowbrick.cluster import KElbowVisualizer
+from yellowbrick.cluster import KElbowVisualizer
 
 
 def synthetic_sampler(df):
@@ -414,7 +414,7 @@ def avg_label(label, spectra, labels): #inputs are an integer label, the array h
                 specs_list.append(spectra[i,:]) #save the spectrum to the list
      
     specs_array=np.array(specs_list) #create an array of the spectra (easier for the following operations)
-    print(specs_array.shape)
+    # print(specs_array.shape)
     #average them up- we want to average along each flux value
     #we construct the averaged spectrum one wavelength at a time
     avg_spec=np.zeros((specs_array.shape[1])) #an empty array to hold each of the flux values
@@ -478,45 +478,45 @@ def normalize_peak(spectrum):
     return normalized_spec
 
 
-def spec_label_get(label, spectra, labels):
-    '''
-    This is a function which, given a label, an array-like containing labels, and an array-
-    like of similar dimension containing the corresponding spectra, returns an array of all
-    the spectra belonging to the specified label class.
+# def spec_label_get(label, spectra, labels):
+#     '''
+#     This is a function which, given a label, an array-like containing labels, and an array-
+#     like of similar dimension containing the corresponding spectra, returns an array of all
+#     the spectra belonging to the specified label class.
 
-    Inputs:
-        label: integer- the chosen label for which the average spectrum is to be plotted
+#     Inputs:
+#         label: integer- the chosen label for which the average spectrum is to be plotted
 
-        spectra: an array-like - an nxm matrix where n is each observation and 
-            m is the wavelength dimension; the array holding the spectra
+#         spectra: an array-like - an nxm matrix where n is each observation and 
+#             m is the wavelength dimension; the array holding the spectra
 
-        labels: an array-like - a 1-D matrix of length n which holds the corresponding 
-            labels for each spectrum in the spectrum matrix
+#         labels: an array-like - a 1-D matrix of length n which holds the corresponding 
+#             labels for each spectrum in the spectrum matrix
 
-    Outputs:
-        an array-like- all the spectra for the specified label class.
-        '''
-    specs_list=[] #an empty list- will hold all the spectra (arrays) for the specified label
+#     Outputs:
+#         an array-like- all the spectra for the specified label class.
+#         '''
+#     specs_list=[] #an empty list- will hold all the spectra (arrays) for the specified label
     
-    #iterate through the labels array
-    for i in range(labels.shape[0]): #across the rows
-        # for j in range(labels.shape[1]): #across the columns
+#     #iterate through the labels array
+#     for i in range(labels.shape[0]): #across the rows
+#         # for j in range(labels.shape[1]): #across the columns
             
-            #save all the corresponding spectra
-            if labels[i]==label:
-                specs_list.append(spectra[i,:]) #save the spectrum to the list
+#             #save all the corresponding spectra
+#             if labels[i]==label:
+#                 specs_list.append(spectra[i,:]) #save the spectrum to the list
      
-    specs_array=np.array(specs_list) #create an array of the spectra (easier for the following operations)
-    # print(specs_array.shape)
-    # #average them up- we want to average along each flux value
-    # #we construct the averaged spectrum one wavelength at a time
-    # avg_spec=np.zeros((specs_array.shape[1])) #an empty array to hold each of the flux values
+#     specs_array=np.array(specs_list) #create an array of the spectra (easier for the following operations)
+#     # print(specs_array.shape)
+#     # #average them up- we want to average along each flux value
+#     # #we construct the averaged spectrum one wavelength at a time
+#     # avg_spec=np.zeros((specs_array.shape[1])) #an empty array to hold each of the flux values
     
-    # for wavelength in range(specs_array.shape[1]): # iterate across the wavelength dimension
-    #     #calculate the average flux value for each wavelength
-    #     avg_flux_value=np.mean(specs_array[:,wavelength]) 
-    #     #now save the flux value in the corresponding position in the container for the averaged spectrum
-    #     avg_spec[wavelength]=avg_flux_value
+#     # for wavelength in range(specs_array.shape[1]): # iterate across the wavelength dimension
+#     #     #calculate the average flux value for each wavelength
+#     #     avg_flux_value=np.mean(specs_array[:,wavelength]) 
+#     #     #now save the flux value in the corresponding position in the container for the averaged spectrum
+#     #     avg_spec[wavelength]=avg_flux_value
         
-    # #now return the averaged spectrum for the given label
-    return specs_array
+#     # #now return the averaged spectrum for the given label
+#     return specs_array